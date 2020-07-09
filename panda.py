11b840331b25b845280bf51f25dde6d81be4b48e
ZarastroR37 <62378412+ZarastroR37@users.noreply.github.com>
2020-07-09 15:39:58 +0200

    Add files via upload

---
+++ panda.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as nd
+from pandas import DataFrame
+
+df = DataFrame({'Chips': ['=====','Simba', 'Lays', ''],
+                'Cooldrink': ['=====','Coke', 'Fanta', ''],
+                'Chocolates': ['=====','Cadbury', 'Tex', ''],
+                'Pies': ['=====','Pepper Steak', 'Chicken', ''],
+                'Fruit': ['=====','Pear', 'Apple', 'Orange'],
+                'Cupcakes': ['=====','Vanilla', 'Chocolate', ''],
+                'Veggies': ['=====','Spinach', 'Cabbage', '']})
+print(df)
